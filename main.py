4ef3c43fe2a9b12b3f9ea070b53e59df9a823364
Shohei Fujii <fujii.shohei@gmail.com>
2017-12-27 17:27:55 +0900

    initial commit

---
+++ main.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+import twitter
+import time
+import os
+import urllib
+import logging
+log = logging.getLogger()
+stream_handle = logging.StreamHandler()
+stream_handle.setLevel(logging.DEBUG)
+log.addHandler(stream_handle)
+log.setLevel(logging.DEBUG)
+from pyshorteners import Shortener
+from io import StringIO
+from dotenv import load_dotenv
+
+#from polyglot.text import Text
+# def extract_words(msg)
+#     tokens = Text(msg)
+#     words = []
+#     for w, t in tokens.pos_tags:
+#         if t in [':
+# 
+#         words.append(w)
+
+
+def shrink_url(url):
+    shortener = Shortener('Bitly', bitly_token=os.environ["BITLY_ACCESS_TOKEN"])
+    return shortener.short(url)
+
+def get_dictionary_url(word):
+    return shrink_url(urllib.parse("https://eow.alc.co.jp/search?q={}".format(word)))
+
+def get_google_image_url(word):
+    return shrink_url(urllib.parse("https://www.google.co.jp/search?tbm=isch&q={}".format(word)))
+
+def main():
+    load_dotenv(os.path.join(os.path.dirname(__file__), 'config'))
+    botname = "words_battle"
+    target_twitterers = ["iw_tatsu", "hamko_intel", "ompugao", "D_Plius"]
+
+    sleep_time = 1
+
+    api = twitter.Api(access_token_key = os.environ["ACCESS_KEY"],
+            access_token_secret = os.environ["ACCESS_SECRET"],
+            consumer_key = os.environ["CONSUMER_KEY"],
+            consumer_secret = os.environ["CONSUMER_SECRET"])
+
+
+    log.info("start!")
+    for tweet in api.GetUserStream():
+        # validate a tweet
+        if "entities" not in tweet:
+            continue
+
+        mentions = tweet["entities"]["user_mentions"]
+        mentioned_users = [ mention["screen_name"] for mention in mentions ]
+
+        log.info("got message: {}".format(tweet.text))
+        if botname in mentioned_users:
+            log.info("thanking @%s for the mention" % tweet["user"]["screen_name"])
+            #words = extract_words(tweet.text)
+            words = tweet.text.split('\n')
+            msg = StringIO()
+            for w in words:
+                msg.write(w)
+                msg.write('\n')
+                msg.write(get_dictionary_url(w))
+                msg.write('\n')
+                msg.write(get_google_image_url(w))
+                msg.write('\n')
+
+            msg.write("from @{}".format(tweet["user"]["screen_name"]))
+            try:
+                api.PostUpdate(msg.getvalue())
+            except Exception as e:
+                log.warning(" - failed (maybe a duplicate?): %s" % e)
+
+        time.sleep(sleep_time)
+
+if __name__ == '__main__':
+    main()
